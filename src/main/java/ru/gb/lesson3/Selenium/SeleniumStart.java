22aaca0030b498ebe656f861f61120516031fd69
Dima <ninehands@yandex.ru>
2022-09-10 18:38:15 +0300

    4444

---
+++ src/main/java/ru/gb/lesson3/Selenium/SeleniumStart.java
@@ -0,0 +1,25 @@
+package ru.gb.lesson3.Selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumStart {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://google.com");
+        driver.close();
+
+
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get("https://ya.ru");
+        firefoxDriver.quit();
+
+
+    }
+}
